4041a9856302d95c5fb2a25b739c888f1d9236af
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-13 15:56:56 +0530

    Customizing HTTP requests

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const request= require('request')
-const url = "http://api.weatherstack.com/current?access_key=0741a92dc32985dd054f1be829b56b30&query=37.8267,-122.4233"
-request({url:url}, (error,response)=>{
-    const data=JSON.parse(response.body)
-    console.log(data.current)
+const url = "http://api.weatherstack.com/current?access_key=0741a92dc32985dd054f1be829b56b30&query=37.8267,-122.4233&units=f"
+request({url:url,json:true}, (error,response)=>{
+    console.log(response.body.current.weather_descriptions[0]+'. It is currently '+response.body.current.temperature+' degrees out.it feels like '+response.body.current.feelslike+' degrees out.')
 })
