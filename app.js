41b44023b186a3ae869f7bf15f66707d9ab181b3
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-15 06:26:36 +0530

    Es6 short hand notation

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const address=process.argv[2]
 if(!address){
     console.log('Please provide an  address')
 }else{
-    geocode(address,(error,data) =>{
+    geocode(address,(error,{latitude,longitude,location}={}) =>{
         if(error){
             return console.log(error)
         }
-        forecast(data.latitude,data.longitude,(error,forecastdata) =>{
+        forecast(latitude,longitude,(error,forecastdata) =>{
             if(error){
                 return console.log(error)
             }
-            console.log(data.location)
+            console.log(location)
             console.log(forecastdata)
         })
     })
