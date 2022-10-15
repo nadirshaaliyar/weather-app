6ffe436f66d2246abcac93cc66847c23a4a9147e
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-15 05:45:28 +0530

    callback chaining v2

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const request= require('request')
 const geocode= require('../weather-app/utils/geocode')
 const forecast=require('../weather-app/utils/forecast')
+const address=process.argv[2]
 
-
- 
-
-
-
-geocode('Kochi,India',(error,data) =>{
-    if(error){
-        return console.log(error)
-    }
-    forecast(data.latitude,data.longitude,(error,forecastdata) =>{
+//console.log(process.argv)
+if(!address){
+    console.log('Please provide an  address')
+}else{
+    geocode(address,(error,data) =>{
         if(error){
             return console.log(error)
         }
-        console.log(data.location)
-        console.log(forecastdata)
+        forecast(data.latitude,data.longitude,(error,forecastdata) =>{
+            if(error){
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastdata)
+        })
     })
-})
+}
