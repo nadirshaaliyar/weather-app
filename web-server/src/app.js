7c257cb307b94f28f624dbb31c467ad8c3fbb697
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-15 17:03:08 +0530

    static file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,29 @@
+const path=require('path')
 const express =require('express')
 
+// console.log(__dirname)
+// console.log((path.join(__dirname,'../public')))
+
+
+const publicDir=path.join(__dirname,'../public')
 const app =express()
 
-app.get('',(req,res)=>{
-    res.send('<h1>Weather</h1>')
+app.use(express.static(publicDir))
 
-})
+
+
+// app.get('',(req,res)=>{
+//     res.send('<h1>Weather</h1>')
+
+// })
 
 app.get('/help',(req,res)=>{
     res.send('Help page')
 })
 
-app.get('/about',(req,res)=>{
-    res.send('<h1>About Page</h1>')
-})
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About Page</h1>')
+// })
 
 app.get('/weather',(req,res)=>{
     res.send({
