3f67249d5053560d7481de009ca6c6faf91a6e3c
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-17 20:03:12 +0530

    structuring

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,7 @@ const forecast=require('./utils/forecast')
 // console.log(__dirname)
 // console.log((path.join(__dirname,'../public')))
 const app=express()
+const port =process.env.PORT || 3000
 
 
 //Define paths for express config
@@ -93,6 +94,6 @@ app.get('*',(req,res)=>{
     
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("The server is up!")
 })
