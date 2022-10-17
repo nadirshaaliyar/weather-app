4ce68e4b78731817a9d89afa35ecf675e2a63807
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2022-10-17 14:13:18 +0530

    search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -10,3 +10,22 @@ fetch('http://localhost:3000/weather?address=Boston').then((response) =>{
         }
     })
 })
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const location=search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response) =>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+})
